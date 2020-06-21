96df5ed2813a0902a2169bd639e9aefca8fb1ac2
Ricky Diep <rdiep@syr.edu>
2020-06-20 22:06:03 -0400

    commit

--- file.js
+++ file.js
@@ -44,10 +44,10 @@ $(document).ready(function(){
 		$this.toggleClass('SeeMore');
 		if($this.hasClass('SeeMore')){
             console.log(this)
-			$this.text('Show More   ').append('<i class="fa fa-angle-double-down" aria-hidden="true"></i>');	
+			$this.text('Show More');	
 		} else {
             console.log(this)
-            $this.html('Less   ').append('<i class="fa fa-angle-double-up" aria-hidden="true"></i>');;		
+            $this.html('Less');		
 		}
 	});
  });
